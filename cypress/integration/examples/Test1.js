916f90e4a8402b212ba49251088633f4c083f288
emreMilon <kurt@milon.com>
2021-12-28 15:02:41 +0100

    test search input key with its results

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,5 +1,10 @@
+/// <reference types="Cypress" />
+
 describe("My first Test Suite", function() {
     it("My FirstTest case", function() {
-        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/") 
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type("ca")
+        cy.wait(2000)
+        cy.get('.product:visible').should('have.length', 4)
     })
 })
