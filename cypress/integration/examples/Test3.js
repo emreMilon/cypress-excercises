3e0b667109883854ebfffefb409dc8cb5feb0358
emreMilon <kurt@milon.com>
2021-12-29 15:55:12 +0100

    add: how to test static and dynamic dropdown

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -6,5 +6,20 @@ describe("My Third Test Suite", function () {
         cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1') // with and we can concat assertion
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
         cy.get("input[type='checkbox']").check(['option2', 'option3'])
+
+        //static dropdown select tag html rule
+
+        cy.get('select').select('option2').should('have.value', 'option2')
+
+        //dynamic dropdown 
+
+        cy.get('#autocomplete').type('ger')
+        cy.get('.ui-menu-item div').each(($el, index, $list) => {
+            if($el.text() === "Germany"){
+                cy.wrap($el).click()
+            }
+        })
+        cy.get('#autocomplete').should('have.value','Germany')
+
     })
 })
