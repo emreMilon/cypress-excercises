7a753357ceba10c22fc5e4990b01b4e0f42b9fed
emreMilon <kurt@milon.com>
2021-12-30 23:26:46 +0100

    building custom commands and parameterize test with multiple data sets

--- cypress/integration/examples/Test9FrameWork.js
+++ cypress/integration/examples/Test9FrameWork.js
@@ -9,6 +9,7 @@ describe("My Framefork Test Suite", function () {
         // runs once before all tests in the block
         // arrow function does not work!!!
         cy.fixture('example.json').then((data) => {
+            this.productName = data.productName
             this.name = data.name
             this.gender= data.gender
         })
@@ -23,6 +24,12 @@ describe("My Framefork Test Suite", function () {
         cy.get(':nth-child(4) > .ng-pristine').should('have.value', this.name)
         cy.get(':nth-child(1) > input.form-control').should('have.attr', "minlength", "2")
         cy.get('#inlineRadio3').should('be.disabled')
+        //building custom cypress commands
+        cy.get(':nth-child(2) > .nav-link').click()
+        this.productName.map((element) => cy.selectProduct(element))
+
+        
+      
 
         
     })
