cf9ea42dfbffa66fb1b3a3d283e00517d2c4f218
emreMilon <kurt@milon.com>
2022-01-04 12:37:44 +0100

    smart tagging in cucumber to execute spesific test

--- cypress/integration/examples/BDD/ecommerce.feature
+++ cypress/integration/examples/BDD/ecommerce.feature
@@ -1,18 +1,18 @@
 Feature: End to end Ecommerce validation
 
     application Regression
-
+    @Regression
     Scenario: Ecommerce products delivery
     Given I open Ecommerce page 
     When I add items to the Cart 
     And Validate the total price 
     Then select the country, submit and verify success message
     
-
+    @Smoke
     Scenario: Filling the form to shop
     Given I open Ecommerce page 
     When I fill the form detail
     |name | gender |
-    |monica| female|
+    |monica| Female|
     Then validate the forms behavior
     And select the shop page
--- cypress/integration/examples/BDD/ecommerce/eCommStepDef.js
+++ cypress/integration/examples/BDD/ecommerce/eCommStepDef.js
@@ -1,8 +1,14 @@
+
+//npx cypress run --spec cypress/integration/examples/BDD/ecommerce.feature --headed 
+//npx cypress-tags run -e TAGS="@Smoke" --headed
+
+
 import {Given, When, Then, And} from "cypress-cucumber-preprocessor/steps"
 import HomePage from "../../../pageObjects/HomePage";
 import ProductPage from "../../../pageObjects/ProductPage";
 const homePage = new HomePage();
 const productPage = new ProductPage();
+let name;
 
 Given('I open Ecommerce page', () => {
     cy.visit(Cypress.env("url")+ "angularpractice/")
@@ -49,13 +55,14 @@ Then('select the country, submit and verify success message', () => {
 
 
 When('I fill the form detail', function(dataTable) {
+    name = dataTable.rawTable[1][0]
     //[monica, female] after dataTable.rawTable[1]
-    homePage.getEditBox().type(dataTable.rawTable[1][0]);
+    homePage.getEditBox().type(name);
     homePage.getGenderBox().select(dataTable.rawTable[1][1]);
 })
 
 Then('validate the forms behavior', function() {
-    homePage.getToWayDataBinding().should("have.value", this.name);
+    homePage.getToWayDataBinding().should("have.value", name);
     homePage.getEditBox().should("have.attr", "minlength", "2");
     homePage.getEntrepreneaur().should("be.disabled");
 })
